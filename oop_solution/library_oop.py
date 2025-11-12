275880bd3d591b20c282cd73c2c5f2d0cffb1b30
MiniMammos <paramee.sae@ku.th>
2025-11-12 20:46:22 +0700

    Third commit , add Class Member

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -1,3 +1,4 @@
+# oop_solution\library_oop.py
 
 class Book:
     def __init__(self, id, title, author, available_copies, total_copies):
@@ -9,3 +10,14 @@ class Book:
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+
+class Member:
+    def __init__(self, member_id, name, email):
+        self.id = member_id
+        self.name = name
+        self.email = email
+        self.borrowed_books = []
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
